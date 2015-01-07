6a7ea68c5f9726894304b49a76edb8bb3f698fdf
Nicolás Pernas Maradei <nicolas.pernas.maradei@emutex.com>
2015-01-07 17:40:38 +0000

    Improvements to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-cd ~ && \
-ln -s .env-config/bash_profile .profile  && \
+# Any subsequent commands which fail will cause the shell script to exit immediately
+set -e
+
+DIR=`pwd`
+cd ~
+ln -s .env-config/bash_profile .profile
 # git prompt tools
-wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash && \
-wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh && \
-ln -s .env-config/vimrc .vimrc && \
-# vim patogen install
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-wget https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim -O ~/.vim/autoload/pathogen.vim && \
-source .profile  && \
+wget https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
+wget https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh -O ~/.git-prompt.sh
+ln -s .env-config/vimrc .vimrc
+
+# vim pathogen install
+mkdir -p ~/.vim
+cd ~/.vim
+git clone https://github.com/tpope/vim-pathogen
+ln -s vim-pathogen/autoload autoload
+mkdir -p ~/.vim/bundle
+cd bundle
+
+# install additional VIM plugins
+git clone https://github.com/embear/vim-localvimrc.git
+
+source ~/.profile
+
 echo "[include]
         path = ~/.env-config/gitconfig
 [diff]
 #       external = /home/nico/.env-config/external_diff.sh
 [merge]
-        tool = meld" >> ~/.gitconfig && \
-cd -
+        tool = meld" >> ~/.gitconfig
+cd $DIR
 
+# Turn off automatic exit on failure.
+set +e
