afaf43f431c1870dd31df87801dca969bc43afbd
Nicolás Pernas Maradei <npernas@twilio.com>
2020-05-03 17:55:53 +0200

    Update source code pro font installer

--- install_source_code_pro.sh
+++ install_source_code_pro.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
-set -u -e
+cleanup() {
+  rm -rf "$DIR"
+  echo 'Done installing source code pro!'
+}
 
-dir="$( mktemp -d )"
-zip="/tmp/sourcecodepro.zip"
-wget -O "$zip" https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
-unzip "$zip" -d "$dir"
+URL='https://github.com/adobe-fonts/source-code-pro/releases/download/2.030R-ro%2F1.050R-it/source-code-pro-2.030R-ro-1.050R-it.zip'
+DIR="$( mktemp -d )"
+ZIP="$DIR/sourcecodepro.zip"
+
+trap cleanup EXIT
+
+wget -O "$ZIP" "$URL"
+unzip "$ZIP" -d "$DIR"
 mkdir -p ~/.fonts
-cp "$dir"/*/OTF/*.otf ~/.fonts/
+cp "$DIR"/*/OTF/*.otf ~/.fonts/
 fc-cache -f -v
-rm -rf "$zip" "$dir"
-echo 'done installing source code pro!'
